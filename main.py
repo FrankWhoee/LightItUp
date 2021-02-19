b2a6c68170bf10eb8b80cedfc6c5d10dd7cd4306
Frank Hui <huifrank@live.com>
2021-02-18 23:20:58 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from rpi_ws281x import *
+import argparse
+
+LED_COUNT = 300
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+if __name__ == '__main__':
+  strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+  strip.begin()
