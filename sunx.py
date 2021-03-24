fb4a437aad936ccc454c7928285e39997f9f5b3f
Frank Hui <huifrank@live.com>
2021-03-23 20:55:11 -0700

    Fix sunx script to adjust for UTC timezone

--- sunx.py
+++ sunx.py
@@ -2,7 +2,7 @@ from requests import get
 import datetime
 
 def get_sunset_delay():
-    data = get("https://api.sunrise-sunset.org/json?lat=49.204140&lng=-122.788265&formatted=0").json()
-    date_time_obj = datetime.datetime.strptime(data["results"]["sunset"], '%Y-%m-%dT%H:%M:%S+00:00')
+    now = datetime.datetime.now()
+    data = get("https://api.sunrise-sunset.org/json?lat=49.204140&lng=-122.788265&formatted=0&date="+now.strftime("%Y-%m-%d")).json()
+    date_time_obj = datetime.datetime.strptime(data["results"]["sunset"], '%Y-%m-%dT%H:%M:%S%z')
     return date_time_obj.timestamp() - date_time_obj.now().timestamp()
-
