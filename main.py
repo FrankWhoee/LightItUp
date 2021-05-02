b9e3e1f717d45e8b3b2cb8f7cd7e7a0a3b2c9205
Frank Hui <huifrank@live.com>
2021-05-02 15:37:36 -0700

    Add control panel

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from sunx import get_sunset_delay
 import argparse
 from threading import Timer
 from threading import Thread
-from flask import Flask, render_template, Response, send_from_directory, session
+from flask import Flask, render_template, Response, send_from_directory, session, request
 from multiprocessing import Process
 
 instances_of_victor = 0
+
 
 class processNode():
     def __init__(self, process, complete: bool, prev, next, name):
@@ -37,6 +38,7 @@ class processNode():
             mt = Process(target=self.next.helper, name=self.next.name)
             mt.start()
 
+
 class async_discord_thread(Thread):
     def __init__(self):
         Thread.__init__(self)
@@ -55,7 +57,8 @@ class async_discord_thread(Thread):
         self.loop.run_forever()
         instances_of_victor += 1
 
-pnode = processNode(0,True,None,None,"init")
+
+pnode = processNode(0, True, None, None, "init")
 
 app = Flask(__name__)
 app.secret_key = 'Zli6WMDUEboJnp34fzwK'.encode('utf8')
@@ -64,6 +67,11 @@ app.secret_key = 'Zli6WMDUEboJnp34fzwK'.encode('utf8')
 @app.route('/assets/<path>')
 def send_assets(path):
     return send_from_directory('assets', path)
+
+
+@app.route('/assets/icons/<path>')
+def send_icons(path):
+    return send_from_directory('assets/icons', path)
 
 
 @app.route('/css/<path>')
@@ -101,6 +109,58 @@ def momalert():
     return "", 200
 
 
+@app.route('/ambience')
+def ambience():
+    ambtype = request.args["v"]
+    print(ambtype)
+    if ambtype == "light":
+        fill(strip, Color(125, 125, 125))
+    elif ambtype == "off":
+        clear(strip)
+    elif ambtype == "dim":
+        fill(strip, Color(5, 5, 5))
+    elif ambtype == "superdim":
+        clear(strip)
+        for i in range(0, strip.numPixels(), 3):
+            strip.setPixelColor(i, Color(1, 1, 1))
+        strip.show()
+    elif ambtype == "reddim":
+        clear(strip)
+        for i in range(0, strip.numPixels(), 2):
+            strip.setPixelColor(i, Color(1, 0, 0))
+        strip.show()
+    elif ambtype == "party":
+        clear(strip)
+        for i in range(0, int(strip.numPixels() / 5)):
+            strip.setPixelColor(i, Color(255, int(i * 255 / (strip.numPixels() / 5)), 0))
+        for i in range(int(strip.numPixels() / 5), int(strip.numPixels() / 5) * 2):
+            strip.setPixelColor(i,
+                                Color(255 - int((i - (strip.numPixels() / 5)) * 255 / (strip.numPixels() / 5)), 255, 0))
+        for i in range(int(strip.numPixels() / 5) * 2, int(strip.numPixels() / 5) * 3):
+            strip.setPixelColor(i, Color(0, 255, int((i - (strip.numPixels() / 5)*2)  * 255 / (strip.numPixels() / 5))))
+        for i in range(int(strip.numPixels() / 5) * 3, int(strip.numPixels() / 5) * 4):
+            strip.setPixelColor(i, Color(0, 255 - int((i - (strip.numPixels() / 5)*3)  * 255 / (strip.numPixels() / 5)), 255))
+        for i in range(int(strip.numPixels() / 5) * 4, strip.numPixels()):
+            strip.setPixelColor(i, Color(int((i - (strip.numPixels() / 5)*4)  * 255 / (strip.numPixels() / 5)), 0, 255))
+        strip.show()
+    return "", 200
+
+
+@app.route('/signal')
+def signal():
+    sigtype = request.args["v"]
+    print(sigtype)
+    if sigtype == "emergency":
+        fill(strip,Color(255,0,0))
+    elif sigtype == "kitchen":
+        flash(strip,Color(255,125,0),3,0.5)
+    elif sigtype == "dad":
+        flash(strip,Color(0,170,212),3,0.5)
+    elif sigtype == "theater":
+        flash(strip,Color(68,0,170),3,0.5)
+    return "", 200
+
+
 # TODO: Implement light protection
 # if light is >75% brightness, lower it after 5min
 # Add dim mode
@@ -134,7 +194,7 @@ mt_terminate = False
 ADMIN = [194857448673247235, 385297155503685632]
 
 
-def train(strip, length, color, wait_time_ms=50, trailing=Color(0,0,0)):
+def train(strip, length, color, wait_time_ms=50, trailing=Color(0, 0, 0)):
     global mt
     global mt_terminate
     child_process = False
@@ -152,21 +212,23 @@ def train(strip, length, color, wait_time_ms=50, trailing=Color(0,0,0)):
         if not mt.is_alive():
             break
         time.sleep(0.5)
+
     def helper():
         for i in range(length):
-          strip.setPixelColor(i, color)
-          strip.show()
+            strip.setPixelColor(i, color)
+            strip.show()
         for i in range(strip.numPixels()):
-          strip.setPixelColor(i, trailing)
-          strip.setPixelColor(i + length, color)
-          strip.show()
-          time.sleep(wait_time_ms/1000)
+            strip.setPixelColor(i, trailing)
+            strip.setPixelColor(i + length, color)
+            strip.show()
+            time.sleep(wait_time_ms / 1000)
 
     if child_process:
         helper()
     else:
         mt = Process(target=helper, name="train")
         mt.start()
+
 
 def flash(strip, color, count, interval):
     global mt
@@ -186,21 +248,23 @@ def flash(strip, color, count, interval):
         if not mt.is_alive():
             break
         time.sleep(0.5)
+
     def helper():
         for d in range(count):
-          for i in range(strip.numPixels()):
-            strip.setPixelColor(i, color)
-          strip.show()
-          time.sleep(interval)
-          clear(strip)
-          strip.show()
-          time.sleep(interval)
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, color)
+            strip.show()
+            time.sleep(interval)
+            clear(strip)
+            strip.show()
+            time.sleep(interval)
 
     if child_process:
         helper()
     else:
         mt = Process(target=helper, name="flash")
         mt.start()
+
 
 def clear(strip):
     for i in range(strip.numPixels()):
@@ -215,41 +279,44 @@ def fill(strip, color):
 
 
 def fade(strip, color1, color2, interval):
-  global mt
-  global mt_terminate
-  child_process = False
-  while True:
-      if mt_terminate:
-          mt_terminate = False
-          return
-      if mt is None:
-          break
-      try:
-        mt.join(timeout=0)
-      except:
-          child_process = True
-          break
-      if not mt.is_alive():
-          break
-      time.sleep(0.5)
-  def helper():
-    max_diff = max(tuple(np.absolute(np.subtract(color2, color1))))
-    diff = tuple(np.subtract(color2, color1))
-    r = color1[0]
-    g = color1[1]
-    b = color1[2]
-    for i in range(max_diff):
-      fill(strip, Color(int(math.floor(r)),int(math.floor(g)),int(math.floor(b))))
-      r += diff[0]/max_diff
-      g += diff[1] / max_diff
-      b += diff[2] / max_diff
-      time.sleep(interval/max_diff)
-    fill(strip, Color(color2[0], color2[1], color2[2]))
-  if child_process:
-      helper()
-  else:
-    mt = Process(target=helper, name="fade")
-    mt.start()
+    global mt
+    global mt_terminate
+    child_process = False
+    while True:
+        if mt_terminate:
+            mt_terminate = False
+            return
+        if mt is None:
+            break
+        try:
+            mt.join(timeout=0)
+        except:
+            child_process = True
+            break
+        if not mt.is_alive():
+            break
+        time.sleep(0.5)
+
+    def helper():
+        max_diff = max(tuple(np.absolute(np.subtract(color2, color1))))
+        diff = tuple(np.subtract(color2, color1))
+        r = color1[0]
+        g = color1[1]
+        b = color1[2]
+        for i in range(max_diff):
+            fill(strip, Color(int(math.floor(r)), int(math.floor(g)), int(math.floor(b))))
+            r += diff[0] / max_diff
+            g += diff[1] / max_diff
+            b += diff[2] / max_diff
+            time.sleep(interval / max_diff)
+        fill(strip, Color(color2[0], color2[1], color2[2]))
+
+    if child_process:
+        helper()
+    else:
+        mt = Process(target=helper, name="fade")
+        mt.start()
+
 
 def timer(strip, color, total_time):
     global mt
@@ -269,21 +336,22 @@ def timer(strip, color, total_time):
         if not mt.is_alive():
             break
         time.sleep(0.5)
+
     def helper():
         fill(strip, color)
         pixels = strip.numPixels()
         if total_time > pixels:
-          time.sleep(math.ceil(float(total_time) / pixels))
-          for i in range(0, pixels - math.ceil(pixels/float(total_time)), math.ceil(pixels/float(total_time))):
-            for k in range(i,i + math.ceil(pixels/float(total_time))):
-              strip.setPixelColor(k,Color(0,0,0))
-            strip.show()
             time.sleep(math.ceil(float(total_time) / pixels))
+            for i in range(0, pixels - math.ceil(pixels / float(total_time)), math.ceil(pixels / float(total_time))):
+                for k in range(i, i + math.ceil(pixels / float(total_time))):
+                    strip.setPixelColor(k, Color(0, 0, 0))
+                strip.show()
+                time.sleep(math.ceil(float(total_time) / pixels))
         else:
-          for i in range(0, pixels):
-            strip.setPixelColor(i, Color(0, 0, 0))
-            strip.show()
-            time.sleep(float(total_time)/pixels)
+            for i in range(0, pixels):
+                strip.setPixelColor(i, Color(0, 0, 0))
+                strip.show()
+                time.sleep(float(total_time) / pixels)
 
         flash(strip, Color(255, 0, 0), 3, 0.5)
 
@@ -292,6 +360,7 @@ def timer(strip, color, total_time):
     else:
         mt = Process(target=helper, name="timer")
         mt.start()
+
 
 def sunset(strip):
     global mt
@@ -311,23 +380,26 @@ def sunset(strip):
         if not mt.is_alive():
             break
         time.sleep(0.5)
+
     def helper():
-        fade(strip, (255,69,0), (0,0,0),1)
+        fade(strip, (255, 69, 0), (0, 0, 0), 1)
         for i in range(CORNER_LED, strip.numPixels()):
-          strip.setPixelColor(i, Color(255,69,0))
+            strip.setPixelColor(i, Color(255, 69, 0))
         strip.show()
         for k in range(3):
-          for i in range(CORNER_LED, -1, -1):
-            strip.setPixelColor(i, Color(int(255 * (float(i)/CORNER_LED)), int(69 * (float(i)/CORNER_LED)), int(255 * ((CORNER_LED - float(i))/CORNER_LED))))
-            strip.show()
-            time.sleep(0.02)
-          for i in range(CORNER_LED, -1, -1):
-            strip.setPixelColor(i, Color(0, 0, 0))
-            strip.show()
-            time.sleep(0.02)
+            for i in range(CORNER_LED, -1, -1):
+                strip.setPixelColor(i, Color(int(255 * (float(i) / CORNER_LED)), int(69 * (float(i) / CORNER_LED)),
+                                             int(255 * ((CORNER_LED - float(i)) / CORNER_LED))))
+                strip.show()
+                time.sleep(0.02)
+            for i in range(CORNER_LED, -1, -1):
+                strip.setPixelColor(i, Color(0, 0, 0))
+                strip.show()
+                time.sleep(0.02)
 
-        flash(strip,Color(255,69,0), 3,0.5)
-        fill(strip, Color(125,125,125))
+        flash(strip, Color(255, 69, 0), 3, 0.5)
+        fill(strip, Color(125, 125, 125))
+
     t = Timer(get_sunset_delay(), sunset, args=[strip])
     t.start()
     if child_process:
@@ -336,83 +408,87 @@ def sunset(strip):
         mt = Process(target=helper, name="sunset")
         mt.start()
 
+
 @client.event
 async def on_message(message):
-  global mt
-  global mt_terminate
-  if message.author == client.user or not message.content.startswith(prefix):
-    return
-  if prefix in message.content and len(message.content.split(" ")) < 2:
-    command = message.content
-    param = []
-  elif prefix in message.content:
-    command = message.content.split(" ")[0]
-    param = message.content.split(" ")[1:]
-  command = command[1:]
-  if command == "superping":
-    flash(strip, Color(200, 50, 218), 3, 0.5)
-  elif command == "off":
-    if mt != None:
-      if mt.name == "timer" and message.author.id not in ADMIN:
-        await message.channel.send("Non-admin users can not override timer commands.")
+    global mt
+    global mt_terminate
+    if message.author == client.user or not message.content.startswith(prefix):
         return
-      mt.terminate()
-      mt_terminate = True
-    clear(strip)
-  elif command == "light":
-    fill(strip, Color(125, 125, 125))
-  elif command == "dim":
-    fill(strip, Color(5, 5, 5))
-  elif command == "superdim":
-    for i in range(0, strip.numPixels(), 3):
-      strip.setPixelColor(i, Color(1, 1, 1))
-    strip.show()
-  elif command == "reddim":
-    for i in range(0, strip.numPixels(), 2):
-      strip.setPixelColor(i, Color(1, 0, 0))
-    strip.show()
-  elif command == "fill":
-    if len(param) < 3:
-      await message.channel.send("Not enough parameters! Command requires three RGB values. Ex. `fill 255 0 123`")
-    elif len(param) > 3:
-      await message.channel.send("Too many parameters! Command requires three RGB values. Ex. `fill 255 0 123`")
-    else:
-      fill(strip, Color(int(param[0]), int(param[1]), int(param[2])))
-  elif command == "flash":
-    if len(param) < 5:
-      await message.channel.send(
-        "Not enough parameters! Command requires three RGB values, a count, and interval between flashes. Ex. `flash 255 0 0 3 0.5`")
-    elif len(param) > 5:
-      await message.channel.send(
-        "Too many parameters! Command requires three RGB values, a count, and interval between flashes. Ex. `flash 255 0 0 3 0.5`")
-    else:
-      flash(strip, Color(int(param[0]), int(param[1]), int(param[2])), int(param[3]), float(param[4]))
-  elif command == "train":
-    if len(param) < 4:
-      await message.channel.send(
-        "Not enough parameters! Command requires three RGB values, and a length. Ex. `train 255 0 0 5`")
-    elif len(param) > 4:
-      await message.channel.send(
-        "Too many parameters! Command requires three RGB values, and a length. Ex. `train 255 0 0 5`")
-    else:
-      train(strip, int(param[3]), Color(int(param[0]), int(param[1]), int(param[2])))
-  elif command == "fade":
-    if len(param) < 6:
-      await message.channel.send(
-        "Not enough parameters! Command requires two sets of three RGB values. Ex. `fade 255 69 0 0 0 0`")
-    elif len(param) > 6:
-      await message.channel.send(
-        "Too many parameters! Command requires two sets of three RGB values. Ex. `fade 255 69 0 0 0 0`")
-    else:
-      fade(strip, (int(param[0]), int(param[1]), int(param[2])), (int(param[3]), int(param[4]), int(param[5])), 1)
-  elif command == "timer":
-    if len(param) == 1:
-      timer(strip, Color(255, 0, 0), int(param[0]))
-    elif len(param) == 4:
-      timer(strip, Color(int(param[0]), int(param[1]), int(param[2])), int(param[3]))
-    elif (len(param) > 1 and len(param) < 4) or len(param) > 4:
-      await message.channel.send(
-        "Parameter mismatch. Command requires either three RGB values and a duration or just a duration in seconds. Ex. `timer 255 0 0 5` or `timer 5`")
+    if prefix in message.content and len(message.content.split(" ")) < 2:
+        command = message.content
+        param = []
+    elif prefix in message.content:
+        command = message.content.split(" ")[0]
+        param = message.content.split(" ")[1:]
+    command = command[1:]
+    if command == "superping":
+        flash(strip, Color(200, 50, 218), 3, 0.5)
+    elif command == "off":
+        if mt != None:
+            if mt.name == "timer" and message.author.id not in ADMIN:
+                await message.channel.send("Non-admin users can not override timer commands.")
+                return
+            mt.terminate()
+            mt_terminate = True
+        clear(strip)
+    elif command == "light":
+        fill(strip, Color(125, 125, 125))
+    elif command == "dim":
+        fill(strip, Color(5, 5, 5))
+    elif command == "superdim":
+        clear(strip)
+        for i in range(0, strip.numPixels(), 3):
+            strip.setPixelColor(i, Color(1, 1, 1))
+        strip.show()
+    elif command == "reddim":
+        clear(strip)
+        for i in range(0, strip.numPixels(), 2):
+            strip.setPixelColor(i, Color(1, 0, 0))
+        strip.show()
+    elif command == "fill":
+        if len(param) < 3:
+            await message.channel.send("Not enough parameters! Command requires three RGB values. Ex. `fill 255 0 123`")
+        elif len(param) > 3:
+            await message.channel.send("Too many parameters! Command requires three RGB values. Ex. `fill 255 0 123`")
+        else:
+            fill(strip, Color(int(param[0]), int(param[1]), int(param[2])))
+    elif command == "flash":
+        if len(param) < 5:
+            await message.channel.send(
+                "Not enough parameters! Command requires three RGB values, a count, and interval between flashes. Ex. `flash 255 0 0 3 0.5`")
+        elif len(param) > 5:
+            await message.channel.send(
+                "Too many parameters! Command requires three RGB values, a count, and interval between flashes. Ex. `flash 255 0 0 3 0.5`")
+        else:
+            flash(strip, Color(int(param[0]), int(param[1]), int(param[2])), int(param[3]), float(param[4]))
+    elif command == "train":
+        if len(param) < 4:
+            await message.channel.send(
+                "Not enough parameters! Command requires three RGB values, and a length. Ex. `train 255 0 0 5`")
+        elif len(param) > 4:
+            await message.channel.send(
+                "Too many parameters! Command requires three RGB values, and a length. Ex. `train 255 0 0 5`")
+        else:
+            train(strip, int(param[3]), Color(int(param[0]), int(param[1]), int(param[2])))
+    elif command == "fade":
+        if len(param) < 6:
+            await message.channel.send(
+                "Not enough parameters! Command requires two sets of three RGB values. Ex. `fade 255 69 0 0 0 0`")
+        elif len(param) > 6:
+            await message.channel.send(
+                "Too many parameters! Command requires two sets of three RGB values. Ex. `fade 255 69 0 0 0 0`")
+        else:
+            fade(strip, (int(param[0]), int(param[1]), int(param[2])), (int(param[3]), int(param[4]), int(param[5])), 1)
+    elif command == "timer":
+        if len(param) == 1:
+            timer(strip, Color(255, 0, 0), int(param[0]))
+        elif len(param) == 4:
+            timer(strip, Color(int(param[0]), int(param[1]), int(param[2])), int(param[3]))
+        elif (len(param) > 1 and len(param) < 4) or len(param) > 4:
+            await message.channel.send(
+                "Parameter mismatch. Command requires either three RGB values and a duration or just a duration in seconds. Ex. `timer 255 0 0 5` or `timer 5`")
+
 
 @client.event
 async def on_ready():
@@ -423,13 +499,14 @@ async def on_ready():
     flash(strip, Color(0, 255, 0), 3, 0.5)
     clear(strip)
 
+
 if __name__ == '__main__':
-  strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-  strip.begin()
-  clear(strip)
-  fade(strip, (255,255,255),(0,0,0), 1)
-  sunset_timer = Timer(get_sunset_delay(), sunset, args=[strip])
-  sunset_timer.start()
-  flash(strip,Color(255,69,0),2,0.5)
-  discord_thread = async_discord_thread()
-  app.run(host="0.0.0.0")
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    clear(strip)
+    fade(strip, (255, 255, 255), (0, 0, 0), 1)
+    sunset_timer = Timer(get_sunset_delay(), sunset, args=[strip])
+    sunset_timer.start()
+    flash(strip, Color(255, 69, 0), 2, 0.5)
+    discord_thread = async_discord_thread()
+    app.run(host="0.0.0.0")
